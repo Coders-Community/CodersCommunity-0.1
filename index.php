9866f2f5f29096be097346cabcad58038e2c4509
mazurrodev <mazurekuba@gmail.com>
2018-01-29 02:38:25 +0100

    Strona logowania, jeszcze bez php'a

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="pl">
+    <head>
+        <title>CodersCommunity</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    </head>
+    <body>
+        <nav>
+            <img src="img/logo.png" class="logo"/>
+            <h1>CodersCommunity</h1>
+            <button>Zarejestruj się</button>
+        </nav>
+        <div class="page-content">
+            <div class="description">
+                <h2>Witaj na stronie zrzeszającej programistów z całej Polski!</h2>
+                <p class="opis">CodersCommunity to portal społecznościowy, który skupia miłośników kodu. Zarówno tych doświadczonych jak i początkujących, nie ważne w jakim języku programujesz - ważne, że robisz to dobrze. Udostępniamy funkcję dzielenia się kodem, użytkownicy mogą łączyć się w grupy - pod kątem zainteresowań jak i wspólnych projektów. Dla nowych w świecie binarnym przygotowaliśmy specjalną sekcję newbe w której to znajdują się artykuły i linki do nauki poszczególnych języków/technologii. Miłego kodowania!</p>
+                <p class="podpis">~Zespół CC</p>
+            </div>
+            <form action="" method="POST" class="login">
+                <input type="text" placeholder="Login" class="login-text"/>
+                <input type="password" placeholder="Haslo" class="login-text"/>
+                <input type="submit" value="Zaloguj" class="login-btn"/>
+            </form>
+        </div>
+    </body>
+</html>
