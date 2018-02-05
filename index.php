404b969c58fb80513514ad3e38d1413fc212e1b2
mazurrodev <mazurekuba@gmail.com>
2018-02-05 23:15:44 +0100

    hwdp

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html lang="pl">
     <head>
         <title>CodersCommunity</title>
+        <meta charset="utf-8"/>
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
     </head>
     <body>
