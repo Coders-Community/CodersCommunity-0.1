db9eec4b6adb9fc0fe3e94b0d792bf893270f63d
mazurrodev <mazurekuba@gmail.com>
2018-02-06 00:59:12 +0100

    spierdalaj

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     <head>
         <meta charset="utf-8">
         <title>CodersCommunity</title>
-        <meta charset="utf-8"/>
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
     </head>
     <body>
